c899982774660320643a17a722cc9bf22beef523
Harshita.Agarwal <Harshita.Agarwal@CS0100447.cns.com>
2021-06-05 23:35:39 +0530

    Testing purpose

---
+++ restaurants/api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Restaurant,Recipe,Ingredient
+# Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
